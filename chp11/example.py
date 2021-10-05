8de329c55547de92347671e5f9947bc215bc054d
jakoblumpkin <68565746+jakoblumpkin@users.noreply.github.com>
2021-10-05 16:30:10 -0500

    Create example.py

---
+++ chp11/example.py
@@ -0,0 +1,19 @@
+function reticulateSplines(){
+    var useNewAlgorithm = false;
+    // useNewAlgorithm = true; // UNCOMMENT IF YOU ARE WORKING ON THE NEW SR ALGORITHM
+  
+    if( useNewAlgorithm ){
+      return firstFunction();
+    }else{
+      return secondFunction();
+    }
+  }
+
+  
+  function firstFunction(){
+    // current implementation lives here
+  }
+  
+  function secondFunction(){
+    // TODO: implement better SR algorithm
+  }
